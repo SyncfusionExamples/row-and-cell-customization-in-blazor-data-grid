da613b7520e523a662ec11f8903e2e9d0c7d7c4f
Arulraj A <arul.raj47@gmail.com>
2020-10-30 17:28:09 +0530

    Committed the example project.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+
+namespace MyBlazorProject
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your Valid license");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Row and Cell Customization in Blazor Data Grid
+
+A quick-start project that helps you to customize the rows and cells of the Blazor Data Grid control. This project also contains simple code to wrap column data, apply clip modes and grid lines.
+
+Documentation:
+
+https://blazor.syncfusion.com/documentation/datagrid/cell/
+
+https://blazor.syncfusion.com/documentation/datagrid/row/#customize-rows
+
+Online examples:
+https://blazor.syncfusion.com/demos/datagrid/overview
+ 
+## Project prerequisites
+
+Make sure that you have the compatible versions of Visual Studio 2019 and .NET Core SDK 3.1.3 or later version in your machine before starting to work on this project.
+
+## How to run this application?
+
+To run this application, you need to first clone the `row-and-cell-customization-in-blazor-data-grid` repository and then open it in Visual Studio 2019. Now, simply build and run your project to view the output.
